ad56b5a8615c982917149370cfe03c1f1c4aec66
P.Gavrilov <p.gavrilov@spb.ant-inform.ru>
2017-10-12 12:27:08 +0300

    lesson 2 completed

--- src/main/java/ru/reeson2003/my3d/engineTester/MainGameLoop.java
+++ src/main/java/ru/reeson2003/my3d/engineTester/MainGameLoop.java
@@ -3,6 +3,9 @@ package ru.reeson2003.my3d.engineTester;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import ru.reeson2003.my3d.renderEngine.DisplayManager;
+import ru.reeson2003.my3d.renderEngine.Loader;
+import ru.reeson2003.my3d.renderEngine.RawModel;
+import ru.reeson2003.my3d.renderEngine.Renderer;
 
 /**
  * Created by Pavel Gavrilov on 12.10.2017.
@@ -12,15 +15,32 @@ public class MainGameLoop {
     public static final int HEIGHT = 720;
     public static final int FPS = 60;
 
+    private static final float[] VERTICLES = {
+            /*left bottom triangle*/
+            -0.5f, 0.5f, 0f,
+            -0.5f, -0.5f, 0f,
+            0.5f, -0.5f, 0f,
+            /*right top triangle*/
+            0.5f, -0.5f, 0f,
+            0.5f, 0.5f, 0f,
+            -0.5f, 0.5f, 0f
+    };
+
     public static void main(String[] args) {
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
         try {
             DisplayManager.createDisplay(WIDTH, HEIGHT, FPS);
+            RawModel model = loader.loadToVAO(VERTICLES);
             while (!Display.isCloseRequested()) {
+                renderer.prepare();
+                renderer.render(model);
                 DisplayManager.updateDisplay();
             }
         } catch (LWJGLException e) {
             e.printStackTrace();
         }finally {
+            loader.cleanUp();
             DisplayManager.closeDisplay();
         }
     }
---
+++ src/main/java/ru/reeson2003/my3d/renderEngine/Renderer.java
@@ -0,0 +1,23 @@
+package ru.reeson2003.my3d.renderEngine;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
+/**
+ * Created by Pavel Gavrilov on 12.10.2017.
+ */
+public class Renderer {
+    public void prepare() {
+        GL11.glClearColor(1, 0, 0, 1);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+    }
+
+    public void render(RawModel model) {
+        GL30.glBindVertexArray(model.getVaoID());
+        GL20.glEnableVertexAttribArray(0);
+        GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, model.getVertexCount());
+        GL20.glDisableVertexAttribArray(0);
+        GL30.glBindVertexArray(0);
+    }
+}
