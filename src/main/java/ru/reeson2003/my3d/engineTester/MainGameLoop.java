abb1b57a0cf43c0cefcc8f0c8fb883ebb9d612ff
P.Gavrilov <p.gavrilov@spb.ant-inform.ru>
2017-10-12 15:29:40 +0300

    lesson 5 completed

--- src/main/java/ru/reeson2003/my3d/engineTester/MainGameLoop.java
+++ src/main/java/ru/reeson2003/my3d/engineTester/MainGameLoop.java
@@ -6,6 +6,7 @@ import ru.reeson2003.my3d.renderEngine.DisplayManager;
 import ru.reeson2003.my3d.renderEngine.Loader;
 import ru.reeson2003.my3d.renderEngine.RawModel;
 import ru.reeson2003.my3d.renderEngine.Renderer;
+import ru.reeson2003.my3d.shaders.StaticShader;
 
 /**
  * Created by Pavel Gavrilov on 12.10.2017.
@@ -29,19 +30,26 @@ public class MainGameLoop {
     };
 
     public static void main(String[] args) {
-        Loader loader = new Loader();
-        Renderer renderer = new Renderer();
+        Loader loader = null;
+        Renderer renderer = null;
+        StaticShader shader = null;
         try {
             DisplayManager.createDisplay(WIDTH, HEIGHT, FPS, TITLE);
+            loader = new Loader();
+            renderer = new Renderer();
+            shader = new StaticShader();
             RawModel model = loader.loadToVAO(VERTICES, INDICES);
             while (!Display.isCloseRequested()) {
                 renderer.prepare();
+                shader.start();
                 renderer.render(model);
+                shader.stop();
                 DisplayManager.updateDisplay();
             }
         } catch (LWJGLException e) {
             e.printStackTrace();
         } finally {
+            shader.cleanUp();
             loader.cleanUp();
             DisplayManager.closeDisplay();
         }
---
+++ src/main/java/ru/reeson2003/my3d/shaders/StaticShader.java
@@ -0,0 +1,18 @@
+package ru.reeson2003.my3d.shaders;
+
+/**
+ * Created by Pavel Gavrilov on 12.10.2017.
+ */
+public class StaticShader extends ShaderProgram {
+    private static final String VERTEX_FILE = "shaders/vertexShader.vert";
+    private static final String FRAGMENT_FILE = "shaders/fragmentShader.frag";
+
+    public StaticShader() {
+        super(VERTEX_FILE, FRAGMENT_FILE);
+    }
+
+    @Override
+    protected void bindAttributes() {
+        super.bindAttribute(0, "position");
+    }
+}
