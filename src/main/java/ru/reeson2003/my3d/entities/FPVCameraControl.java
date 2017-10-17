7e110db4cfdf559ec17b931f0479ffe7a2611d25
P.Gavrilov <p.gavrilov@spb.ant-inform.ru>
2017-10-17 16:23:19 +0300

    lesson 14 completed

--- src/main/java/ru/reeson2003/my3d/entities/FPVCameraControl.java
+++ src/main/java/ru/reeson2003/my3d/entities/FPVCameraControl.java
@@ -35,12 +35,12 @@ public class FPVCameraControl implements CameraControl {
             position.x -= speed * Math.sin(Math.toRadians(yawPitchRoll.getX()));
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-            position.z -= speed * (Math.cos(Math.toRadians(yawPitchRoll.getX()) - Math.PI/2d));
-            position.x += speed * (Math.sin(Math.toRadians(yawPitchRoll.getX()) - Math.PI/2d));
+            position.z -= speed * (Math.cos(Math.toRadians(yawPitchRoll.getX()) - Math.PI / 2d));
+            position.x += speed * (Math.sin(Math.toRadians(yawPitchRoll.getX()) - Math.PI / 2d));
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-            position.z += speed * (Math.cos(Math.toRadians(yawPitchRoll.getX()) - Math.PI/2d));
-            position.x -= speed * (Math.sin(Math.toRadians(yawPitchRoll.getX()) - Math.PI/2d));
+            position.z += speed * (Math.cos(Math.toRadians(yawPitchRoll.getX()) - Math.PI / 2d));
+            position.x -= speed * (Math.sin(Math.toRadians(yawPitchRoll.getX()) - Math.PI / 2d));
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
             position.y += speed;
@@ -58,10 +58,19 @@ public class FPVCameraControl implements CameraControl {
             int dx = Mouse.getDX();
             yawPitchRoll.x += dx * speed;
             int dy = Mouse.getDY();
-            yawPitchRoll.y += dy * speed;
+            increasePitch(dy);
             int dz = Mouse.getDWheel();
             yawPitchRoll.z += dz * speed;
         }
+    }
+
+    private void increasePitch(int delta) {
+        float res = yawPitchRoll.y + delta * speed;
+        if (res > 90)
+            res = 90;
+        else if (res < -90)
+            res = -90;
+        yawPitchRoll.y = res;
     }
 
     @Override
