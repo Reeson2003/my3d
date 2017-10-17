9c2502cb2aeeb6638737eaeb59ab6182069c4338
P.Gavrilov <p.gavrilov@spb.ant-inform.ru>
2017-10-17 10:47:14 +0300

    lesson 14 completed

--- src/main/java/ru/reeson2003/my3d/engineTester/MainGameLoop.java
+++ src/main/java/ru/reeson2003/my3d/engineTester/MainGameLoop.java
@@ -36,33 +36,34 @@ public class MainGameLoop {
             DisplayManager.createDisplay(WIDTH, HEIGHT, FPS, TITLE);
             loader = new Loader();
             shader = new StaticShader();
-            RawModel model = OBJLoader.loadModel("models/dragon/dragon.obj", loader);
-            ModelTexture texture = new ModelTexture(loader.loadTexture("models/dragon/dragon.png"));
-            texture.setReflectivity(10f);
-            texture.setShineDamper(50f);
+            RawModel model = OBJLoader.loadModel("models/tree/tree.obj", loader);
+            ModelTexture texture = new ModelTexture(loader.loadTexture("models/tree/tree.png"));
+            texture.setReflectivity(1f);
+            texture.setShineDamper(100f);
             TexturedModel texturedModel = new TexturedModel(model, texture);
-            Entity entity = new Entity(texturedModel, new Vector3f(100, 0, 100), 0, 0, 0, 1);
+//            Entity entity = new Entity(texturedModel, new Vector3f(100, 0, 100), 0, 0, 0, 1);
             Light cold = new Light(new Vector3f(3000, 2000, 2000), new Vector3f(1f, 1f, 1f));
-            Terrain terrain = new Terrain(0,0, loader, new ModelTexture(loader.loadTexture("textures/grass.png")));
-            Terrain terrain2 = new Terrain(1,0, loader, new ModelTexture(loader.loadTexture("textures/grass.png")));
-//            Random random = new Random(System.nanoTime());
-//            List<Entity> entities = new ArrayList<>();
-//            for (int i = 0; i < 20; i++) {
-//                entities.add(generateEntity(texturedModel, random));
-//            }
-            Camera camera = new Camera(CAMERA_SPEED, new Vector3f(120,10,150), new Vector3f(-30, 0, 0));
+            ModelTexture grass = new ModelTexture(loader.loadTexture("textures/grass.png"));
+            Terrain terrain = new Terrain(0, 0, loader, grass);
+            Terrain terrain2 = new Terrain(1, 0, loader, grass);
+            Random random = new Random(System.nanoTime());
+            List<Entity> entities = new ArrayList<>();
+            for (int i = 0; i < 100; i++) {
+                entities.add(generateEntity(texturedModel, random));
+            }
+            Camera camera = new Camera(CAMERA_SPEED, new Vector3f(120, 10, 150), new Vector3f(-30, 0, 0));
 
             MasterRenderer masterRenderer = new MasterRenderer();
             while (!Display.isCloseRequested()) {
                 camera.move();
-//                for (Entity entity : entities) {
-//                    masterRenderer.processEntity(entity);
+                for (Entity entity : entities) {
+                    masterRenderer.processEntity(entity);
 //                    entity.increasePosition(0,0,-10f);
 //                    entity.increaseRotation(1f, 1f, 0);
-//                }
+                }
                 masterRenderer.processTerrain(terrain);
                 masterRenderer.processTerrain(terrain2);
-                masterRenderer.processEntity(entity);
+//                masterRenderer.processEntity(entity);
                 masterRenderer.render(cold, camera);
                 DisplayManager.updateDisplay();
             }
@@ -79,17 +80,21 @@ public class MainGameLoop {
     }
 
     private static Entity generateEntity(TexturedModel model, Random random) {
-        final float tMax = 100f;
-        final float tMin = -100f;
+        final float tMax = 800f;
+        final float tMin = 0f;
         float tx = getRandomFloat(random, tMin, tMax);
-        float ty = getRandomFloat(random, tMin, tMax);
+//        float ty = getRandomFloat(random, tMin, tMax);
+        float ty = 0;
         float tz = getRandomFloat(random, tMin, tMax);
         Vector3f v = new Vector3f(tx, ty, tz);
         final float rMax = 100f;
         final float rMin = -100f;
-        float rx = getRandomFloat(random, rMin, rMax);
-        float ry = getRandomFloat(random, rMin, rMax);
-        float rz = getRandomFloat(random, rMin, rMax);
+//        float rx = getRandomFloat(random, rMin, rMax);
+//        float ry = getRandomFloat(random, rMin, rMax);
+//        float rz = getRandomFloat(random, rMin, rMax);
+        float rx = 0;
+        float ry = 0;
+        float rz = 0;
         float sMax = 5f;
         float sMin = 5F;
         float s = getRandomFloat(random, sMin, sMax);
