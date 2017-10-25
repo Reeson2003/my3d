9b48dec10d5d9535f5d85147e9739653315ebdf1
P.Gavrilov <p.gavrilov@spb.ant-inform.ru>
2017-10-25 13:36:31 +0300

    sorted map

--- my3dclient/src/main/java/ru/reeson2003/my3d/client/models/ModelLoader.java
+++ my3dclient/src/main/java/ru/reeson2003/my3d/client/models/ModelLoader.java
@@ -4,13 +4,14 @@ import ru.reeson2003.my3d.client.renderEngine.Loader;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.SortedMap;
 
 
 /**
  * Created by Pavel Gavrilov on 25.10.2017.
  */
 public interface ModelLoader {
-    Map<String, TexturedModel> getModels(Loader loader) throws IOException;
+    SortedMap<String, TexturedModel> getModels(Loader loader) throws IOException;
 
     TexturedModel getModel(String id);
 }
